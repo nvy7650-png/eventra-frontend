83af9c1ce2124c6006a801cfe5e02c37fd929f78
Vy <nvy7650@gmail.com>
2026-05-21 17:37:49 +0700

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import SeatMap from "./pages/SeatMap";
 import OrganizerRegister from "./pages/OrganizerRegister";
 import OrganizerDashboard from "./pages/OrganizerDashboard";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/organizerregister" element={<OrganizerRegister />} />
-        <Route path="/organizer/dashboard" element={<OrganizerDashboard />} />
 
-<Route
-  path="/events/:id/seats"
-  element={<SeatMap />}
-/>
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/organizerregister"
+          element={<OrganizerRegister />}
+        />
+
+        <Route
+          path="/organizer/dashboard"
+          element={<OrganizerDashboard />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
       </Routes>
-      
+
     </BrowserRouter>
+
   );
+
 }
 
 export default App;
