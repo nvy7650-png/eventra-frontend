177fc5d6f9034acc742883d2156d03410087c60c
Vy <nvy7650@gmail.com>
2026-06-19 10:41:58 +0700

    add organizer sidebar layout

--- src/pages/MyTickets.jsx
+++ src/pages/MyTickets.jsx
@@ -227,35 +227,6 @@ font-semibold
 >
   {ticket.event_title}
 </h3>
-<div className="mb-6">
-
-  <span
-    className={`
-      px-4
-      py-2
-      rounded-full
-      text-sm
-      font-semibold
-
-      ${
-        ticket.status === "VALID"
-          ? "bg-green-500/20 text-green-400"
-          : ticket.status === "USED"
-          ? "bg-gray-500/20 text-gray-300"
-          : "bg-red-500/20 text-red-400"
-      }
-    `}
-  >
-    {
-      ticket.status === "VALID"
-        ? "Còn hiệu lực"
-        : ticket.status === "USED"
-        ? "Đã sử dụng"
-        : "Đã hủy"
-    }
-  </span>
-
-</div>
 
              <div
   className="
