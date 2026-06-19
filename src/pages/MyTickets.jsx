c59ee75ba59a1d68e4afd90b6b1e56b3c4bc5fd0
Vy <nvy7650@gmail.com>
2026-06-19 10:19:01 +0700

    add organizer sidebar layout

--- src/pages/MyTickets.jsx
+++ src/pages/MyTickets.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function MyTickets() {
 
@@ -35,11 +36,30 @@ export default function MyTickets() {
 
   return (
 
-    <div className="min-h-screen bg-gray-950 text-white">
+   <div className="min-h-screen bg-[#050816] text-white">
 
-      <div className="max-w-5xl mx-auto py-12 px-6">
+  <Navbar />
 
-        <h1 className="text-4xl font-bold text-sky-400 mb-8">
+      <div
+  className="
+    max-w-6xl
+    mx-auto
+    py-8
+    md:py-12
+    px-4
+    md:px-6
+  "
+>
+
+        <h1
+  className="
+    text-2xl
+    md:text-4xl
+    font-black
+    text-sky-400
+    mb-8
+  "
+>
           Vé của tôi
         </h1>
 
@@ -54,19 +74,26 @@ export default function MyTickets() {
             </div>
           )}
 
-        <div className="grid gap-6">
+        <div
+  className="
+    grid
+    grid-cols-1
+    lg:grid-cols-2
+    gap-6
+  "
+>
 
           {tickets.map((ticket) => (
 
             <div
               key={ticket.id}
               className="
-                bg-gray-900
-                border
-                border-gray-800
-                rounded-3xl
-                p-6
-              "
+  bg-[#0B1220]
+  border
+  border-white/10
+  rounded-3xl
+  overflow-hidden
+"
             >
 
               <div className="text-2xl font-bold text-sky-400 mb-3">
@@ -91,9 +118,31 @@ export default function MyTickets() {
               <div className="mb-2">
                 Trạng thái:
                 {" "}
-                <span className="text-green-400">
-                  {ticket.status}
-                </span>
+                <span
+  className={`
+    px-3
+    py-1
+    rounded-full
+    text-xs
+    font-bold
+
+    ${
+      ticket.status === "VALID"
+        ? "bg-green-500/20 text-green-400"
+        : ticket.status === "USED"
+        ? "bg-gray-500/20 text-gray-300"
+        : "bg-red-500/20 text-red-400"
+    }
+  `}
+>
+  {
+    ticket.status === "VALID"
+      ? "Còn hiệu lực"
+      : ticket.status === "USED"
+      ? "Đã sử dụng"
+      : "Đã hủy"
+  }
+</span>
               </div>
 
               <button
@@ -101,14 +150,16 @@ export default function MyTickets() {
     navigate(`/ticket/${ticket.id}`)
   }
   className="
-    mt-4
-    px-5
-    py-3
-    rounded-xl
-    bg-sky-500
-    text-black
-    font-bold
-  "
+  mt-5
+  w-full
+  py-3
+  rounded-2xl
+  bg-sky-500
+  hover:bg-sky-400
+  text-black
+  font-bold
+  transition
+"
 >
   Xem vé
 </button>
