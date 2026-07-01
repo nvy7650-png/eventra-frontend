e4bd5d95932e4f4a7c7f5ed382cd3b8a6693f61c
Vy <nvy7650@gmail.com>
2026-07-01 08:59:10 +0700

    add organizer sidebar layout

--- src/pages/AutoBooking.jsx
+++ src/pages/AutoBooking.jsx
@@ -86,14 +86,18 @@ const [selectedShowtime,
     }));
 
   };
-  const currentZones =
-  console.log("SHOWTIME:", selectedShowtime);
-console.log("ZONES:", zones);
-  zones.filter(
-    (z) =>
-      z.showtime_id ===
-      selectedShowtime
-  );
+  console.log(
+  "SHOWTIME:",
+  selectedShowtime
+);
+
+console.log(
+  "ZONES:",
+  zones
+);
+
+const currentZones =
+  zones;
 
   const totalQuantity =
     Object.values(
